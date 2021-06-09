8a2505fe9039d8ad111d802bafb49d47e570918b
reflexdemon <venkateswara.vp@gmail.com>
2021-06-09 07:25:01 -0400

    Added ES Lint and fixed linting errors

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-page.component.scss']
 })
 export class AboutPageComponent implements OnInit {
-
+  variable = 0;
   constructor() { }
 
   ngOnInit(): void {
+    this.variable = 1;
   }
 
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
-
+  variable = 0
   constructor() { }
 
   ngOnInit(): void {
+    this.variable = 1;
   }
 
 }
