1e7c9f86c6921ecc3a1ca8903cddaf93f5b6b10b
reflexdemon <venkateswara.vp@gmail.com>
2021-06-09 07:16:06 -0400

    Got the router Link working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'tele-spell';
+export class AppComponent implements OnInit, OnDestroy {
+  active = 0;
+  collapse = true;
+  fragment:string|null  ='';
+  private subscriptions: Subscription[] = [];
+
+  links = [
+    { title: 'Home', fragment: '' },
+    { title: 'About', fragment: 'about' }
+    
+  ];
+
+
+  constructor(public route: ActivatedRoute, private router: Router) {
+  }
+  ngOnInit() {
+    this.router.navigate([this.links[0].fragment], { });
+  }
+
+  toggleCollapse(indexValue:number):void {
+    this.collapse = !this.collapse;
+    this.active = indexValue;
+  }
+
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
 }
