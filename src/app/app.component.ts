ed03dbd3444d73559a3eea1212507bbfb557fc17
Venkateswara VP <venkateswara.vp@gmail.com>
2021-07-07 09:36:20 -0400

    Optimize imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscriber, Subscription } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
